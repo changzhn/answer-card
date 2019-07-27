31e9014e02cae996c9ec4e1e7c39bb4b40cabdd4
changzhn <changzhenan0212@gmail.com>
2019-07-27 21:16:57 +0800

    作文题样式

--- src/components/QuestionComponents/EssayQuestion/index.tsx
+++ src/components/QuestionComponents/EssayQuestion/index.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import styles from './index.css';
+
+const { wrapper, blocksWrapper, blockRowWrapper, blockSquare, blockBlankRow } = styles;
 
 export default class EssayQuestion extends React.Component {
   render() {
     return (
-      <div>x</div>
+      <div className={wrapper}>
+        <div className={blocksWrapper}>
+          <div className={blockRowWrapper}>
+            {new Array(21).fill(0).map((_, idx) => <span key={idx} className={blockSquare} />)}
+          </div>
+          <div className={blockBlankRow} />
+        </div>
+      </div>
     )
   }
 }
