0f4579bc0cd3a6a6bf8b084ee66f01568b9b6418
joo-fanchang <changzhenan0212@gmail.com>
2019-09-17 08:40:08 +0800

    answer-question题号

--- src/components/SvgShape/QuestionComponents/AnswerQuestion/index.tsx
+++ src/components/SvgShape/QuestionComponents/AnswerQuestion/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import AnswerQuestionClass from '@/tools/QuestionClasses/AnswerQuestionClass';
+import Text from '../../Text';
 
 export interface IProps {
   component: AnswerQuestionClass;
@@ -9,11 +10,20 @@ export interface IProps {
 
 export default class AnswerQuestion extends React.Component<IProps> {
   public render() {
-    console.log(this.props)
     const { component, colWidth } = this.props;
     const { requiredHeight, offsetY } = component;
     return (
-      <rect x="0" y={offsetY} width={colWidth} height={requiredHeight} fill="transparent" stroke="#000" strokeWidth="0.2" />
+      <g transform={`translate(0, ${offsetY})`}>
+        <rect x="0" y="0" width={colWidth} height={requiredHeight} fill="transparent" stroke="#000" strokeWidth="0.2" />
+        <Text 
+          x={2}
+          y={2}
+          width={2}
+          height={2}
+          text={`${component.question.questionNo}、`}
+          align="center"
+        />
+      </g>
     );
   }
 }
