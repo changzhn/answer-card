db42955347acbafe8eaa64c1090a2736c7fbd310
joo-fanchang <changzhenan0212@gmail.com>
2019-09-19 09:09:45 +0800

    feat: Text增加border选项

--- src/components/SvgShape/Text/index.tsx
+++ src/components/SvgShape/Text/index.tsx
@@ -5,12 +5,13 @@ interface IProps {
   y: number;
   width: number;
   height: number;
-  text: string;
+  text: string | number;
   align: 'center' | 'left';
   fontSize?: string;
+  border?: boolean;
 }
 
-const Text: React.SFC<IProps> = ({ x, y, width, height, text, align, fontSize = '3pt' }) => {
+const Text: React.SFC<IProps> = ({ x, y, width, height, text, align, fontSize = '3pt', border = false }) => {
   const extraStyle: any = {
     dominantBaseline: 'middle',
   };
@@ -18,10 +19,19 @@ const Text: React.SFC<IProps> = ({ x, y, width, height, text, align, fontSize = 
     extraStyle.textAnchor = 'middle';
   }
 
+  let borderStyle = {};
+
+  if (border) {
+    borderStyle = {
+      stroke: '#000',
+      strokeWidth: '0.2',
+    };
+  }
+
   const fz = parseInt(fontSize);
   return (
     <g transform={`translate(${x}, ${y})`}>
-      <rect x="0" y="0" width={width} height={height} fill="transparent" />
+      <rect x="0" y="0" width={width} height={height} fill="transparent" {...borderStyle} />
       <text
         x={align === 'center' ? width / 2 : 0}
         y={fz * 0.9 * (1.35 / 3) + ((height - 5) / 2)}
