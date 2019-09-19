bac24474a1a1818e2c5e919b25249abec882492b
joo-fanchang <changzhenan0212@gmail.com>
2019-09-19 08:25:42 +0800

    bugfix: 文字垂直居中

--- src/components/SvgShape/Text/index.tsx
+++ src/components/SvgShape/Text/index.tsx
@@ -18,11 +18,13 @@ const Text: React.SFC<IProps> = ({ x, y, width, height, text, align, fontSize = 
     extraStyle.textAnchor = 'middle';
   }
 
+  const fz = parseInt(fontSize);
   return (
-    <g transform={`translate(${x} ${y})`}>
+    <g transform={`translate(${x}, ${y})`}>
       <rect x="0" y="0" width={width} height={height} fill="transparent" />
       <text
         x={align === 'center' ? width / 2 : 0}
+        y={fz * 0.9 * (1.35 / 3) + ((height - 5) / 2)}
         width={width}
         height={height}
         style={{
